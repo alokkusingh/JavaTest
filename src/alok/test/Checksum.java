6aa3dbaff9cfe33bce6cf66ef5c4de8010a031b8
alokkusingh <alok.ku.singh@gmail.com>
2018-01-08 15:02:05 +0530

    Removed co right

--- src/alok/test/Checksum.java
+++ src/alok/test/Checksum.java
@@ -1,57 +1,5 @@
-/******************************************************************************
-#
-#	%name: Checksum.java %
-#	%derived_by: alok %
-#
-#	%release: Development/1.1.0 %
-#	%version: 1 %
-#	%date_modified: 3:33:26 PM %
-#
-#	Description: Cassandra DB PA. %
-#  Change History: None. %
-#
-#  Date        By                        Description
-#  ----        -----------------------   -----------
-#  24/07/2017  alok.singh@evolving.com   Cassnadra DB PA
-#
-#*****************************************************************************
-# Copyright (c) 2017 Evolving Systems Ltd
-# All Rights Reserved.
-#
-# The copyright to this program herein is the property of
-# Evolving Systems Ltd. The program may be used and/or copied only with
-# written permission of Evolving Systems Ltd or in accordance with the
-# terms and conditions stipulated in the agreement/contract under
-# which the program has been supplied.
-#*****************************************************************************/
 package alok.test;
 
-/******************************************************************************
-#
-#	%name: Checksum.java %
-#	%derived_by: alok %
-#
-#	%release: Development/1.1.0 %
-#	%version: 1 %
-#	%date_modified: 3:33:26 PM %
-#
-#	Description: Cassandra DB PA. %
-#  Change History: None. %
-#
-#  Date        By                        Description
-#  ----        -----------------------   -----------
-#  24/07/2017  alok.singh@evolving.com   Cassnadra DB PA
-#
-#*****************************************************************************
-# Copyright (c) 2017 Evolving Systems Ltd
-# All Rights Reserved.
-#
-# The copyright to this program herein is the property of
-# Evolving Systems Ltd. The program may be used and/or copied only with
-# written permission of Evolving Systems Ltd or in accordance with the
-# terms and conditions stipulated in the agreement/contract under
-# which the program has been supplied.
-#*****************************************************************************/
 public class Checksum {
 
 	/**
