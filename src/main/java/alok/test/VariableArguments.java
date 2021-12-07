44f5358336acad3fce587d4831a4b343c575f6a0
alokkusingh <alok.ku.singh@gmail.com>
2021-12-07 09:42:03 +0000

    updated copyright statement

--- src/main/java/alok/test/VariableArguments.java
+++ src/main/java/alok/test/VariableArguments.java
@@ -12,18 +12,9 @@
 #
 #  Date        By                        Description
 #  ----        -----------------------   -----------
-#  24/07/2017  alok.singh@evolving.com   Cassnadra DB PA
+#  24/07/2017  alok.ku.singh@gmail.com   Cassnadra DB
 #
-#*****************************************************************************
-# Copyright (c) 2017 Evolving Systems Ltd
-# All Rights Reserved.
-#
-# The copyright to this program herein is the property of
-# Evolving Systems Ltd. The program may be used and/or copied only with
-# written permission of Evolving Systems Ltd or in accordance with the
-# terms and conditions stipulated in the agreement/contract under
-# which the program has been supplied.
-#*****************************************************************************/
+
 package alok.test;
 
 public class VariableArguments {
